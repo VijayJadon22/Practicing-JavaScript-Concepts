6695dc8a6e3a5bf6bcee26b2337096918ea15df1
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-30 19:03:22 +0530

    Implemnted block scoped variables

--- app.js
+++ app.js
@@ -6,9 +6,18 @@
 
 // console.log(a, b, c);
 
-function myFunction() {
-    let localVar = "I am local";
-    console.log("Inside Funtion",localVar);
+// function myFunction() {
+//     let localVar = "I am local";
+//     console.log("Inside Funtion",localVar);
+// }
+// myFunction();
+// console.log("Outside Funtion",localVar);
+
+{
+    var a = "Var type";
+    let b = "let type";
+    const c = "const type";
+    console.log(a, b, c);
 }
-myFunction();
-console.log("Outside Funtion",localVar);
+// console.log(a, b, c);  /* will throw an error b is not defined as let and const are block scope and cannot be accessed outside block but var can be accessed outside block*/
+console.log(a);
