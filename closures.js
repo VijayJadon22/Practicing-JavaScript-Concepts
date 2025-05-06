aa83de03a0b67959dbcbf670993a1bf7b5728a3b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-06 19:01:53 +0530

    Implemnted Web API Geolocation to get user current position

--- closures.js
+++ closures.js
@@ -10,3 +10,7 @@ const counter = outerFunction();
 counter();
 counter();
 counter();
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(`Latitude: ${position.coords.latitude} , Longitude: ${position.coords.longitude}, Altitude: ${position.coords.altitude}`)
+})
