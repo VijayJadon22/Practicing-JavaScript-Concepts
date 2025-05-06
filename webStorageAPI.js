aacfa9f5bc1e4d72f2a2eb553bbb0ffb84dd580b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-06 19:49:53 +0530

    Implemnted Web storage APIS like localstorage and session storage

---
+++ webStorageAPI.js
@@ -0,0 +1,7 @@
+import data from "./data.js"
+// localStorage.setItem("users", JSON.stringify(data));
+
+// const list = JSON.parse((localStorage.getItem("users")));
+// console.log(list);
+
+// sessionStorage.setItem("users", JSON.stringify(data));
