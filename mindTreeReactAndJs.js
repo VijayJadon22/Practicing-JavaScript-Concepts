b435dce1a695300282349378295ff55955a3c913
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-07 23:19:20 +0530

    Implemnted another question asked

--- mindTreeReactAndJs.js
+++ mindTreeReactAndJs.js
@@ -1,4 +1,14 @@
-console.log(1);
-setTimeout(() => console.log(2), 1000);
-setTimeout(() => console.log(3), 0);
-console.log(4);
+// console.log(1);
+// setTimeout(() => console.log(2), 1000);
+// setTimeout(() => console.log(3), 0);
+// console.log(4);
+
+
+function x() {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000)
+    var i = 1;
+}
+
+x();
