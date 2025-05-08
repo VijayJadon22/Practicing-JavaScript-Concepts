1d6b378fdb0e0eadcb43c022c9f1b094f5e585db
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-09 00:34:59 +0530

    Implemnted and revised Context API

--- React Questions Practice/src/App.jsx
+++ React Questions Practice/src/App.jsx
@@ -1,32 +1,43 @@
-import { useReducer } from "react";
+// import { useReducer } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Cart from "./components/Cart.jsx";
+import { useCartContext } from "./context/CartContext.jsx";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "INCREMENT": {
-      return { count: state.count + 1 };
-    }
-    case "DECREMENT": {
-      return { count: state.count - 1 };
-    }
-    case "RESET": {
-      return { count: 0 };
-    }
-  }
-};
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "INCREMENT": {
+//       return { count: state.count + 1 };
+//     }
+//     case "DECREMENT": {
+//       return { count: state.count - 1 };
+//     }
+//     case "RESET": {
+//       return { count: 0 };
+//     }
+//   }
+// };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { setCount } = useCartContext();
   return (
     <div>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>
+      {/* <button onClick={() => dispatch({ type: "INCREMENT" })}>
         Increment Count
       </button>
       <h2>Count is : {state.count}</h2>
       <button onClick={() => dispatch({ type: "DECREMENT" })}>
         Decrement Count
       </button>
-      <button onClick={() => dispatch({ type: "RESET" })}>Reset Count</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset Count</button> */}
+      <Navbar />
+      <Cart />
+      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+      <button onClick={() => setCount((count) => (count < 1 ? 0 : count - 1))}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
