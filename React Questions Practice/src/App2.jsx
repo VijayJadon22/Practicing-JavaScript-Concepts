500383fa34df613806d6bc09f87d764237eaa296
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-14 23:36:03 +0530

    Imported Footer to implemnt in App2

--- React Questions Practice/src/App2.jsx
+++ React Questions Practice/src/App2.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Footer from "./components/Footer";
 
 const App2 = () => {
-    return (<div>
-      <h1>Hello from App2</h1>
-  </div>);
+  return (
+    <div>
+      <h1>Hello from App2 creating context</h1>
+      <Footer />
+    </div>
+  );
 };
 
 export default App2;
